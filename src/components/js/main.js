aa62577305fe40869b1005f2f9ff5c82973f9b05
Ros Yan <ros.yanikin@gmail.com>
2025-01-13 19:11:23 +0000

    Rostyslav footer and section-3

--- src/components/js/main.js
+++ src/components/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    $('.section-3__slider').slick({
+        prevArrow: '<img class="section-3__slider-arrow slider__arrow-left" src="../img/section3/arrow-left.svg" alt="">',
+        nextArrow: '<img class="section-3__slider-arrow slider__arrow-right" src="../img/section3/arrow-right.svg" alt="">',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+    });
 });
