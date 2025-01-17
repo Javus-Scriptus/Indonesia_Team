831db3e61029c7f1bb6a0a1f55e46d09d7bb6288
Ros Yan <ros.yanikin@gmail.com>
2025-01-17 16:02:30 +0000

    new

--- src/components/js/main.js
+++ src/components/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.section-3__slider').slick({
-        prevArrow: '<img class="section-3__slider-arrow slider__arrow-left" src="../img/section3/arrow-left.svg" alt="">',
-        nextArrow: '<img class="section-3__slider-arrow slider__arrow-right" src="../img/section3/arrow-right.svg" alt="">',
+        prevArrow: '<img class="section-3__slider-arrow slider__arrow-left" src="/src/img/section3/arrow-left.svg" alt="">',
+        nextArrow: '<img class="section-3__slider-arrow slider__arrow-right" src="/src/img/section3/arrow-right.svg" alt="">',
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
