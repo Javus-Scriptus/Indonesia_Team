41c824cc909f2b0692f3f3955d10bf04b98ffecc
Ros Yan <ros.yanikin@gmail.com>
2025-01-18 12:55:07 +0000

    new

--- src/components/js/main.js
+++ src/components/js/main.js
@@ -6,5 +6,38 @@ $(function(){
         slidesToShow: 3,
         slidesToScroll: 3, 
         dots: true,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                prevArrow:  '<img class="section3-slider__arrow slider__arrow-left" src="" alt="">',
+                nextArrow: '<img class="section3-slider__arrow slider__arrow-left" src="" alt="">' ,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
+                prevArrow: false,
+                nextArrow: false,
+              }
+            },
+            {
+              breakpoint: 820,
+              settings: {
+                slidesToShow: 1,
+                prevArrow:  '<img class="section3-slider__arrow slider__arrow-left" src="" alt="">',
+                nextArrow: '<img class="section3-slider__arrow slider__arrow-left" src="" alt="">' ,
+                dots: false,
+                autoplay: true,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 });
