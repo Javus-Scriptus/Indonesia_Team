3e74e7587641b0202e0e69d2e64678445809fc0d
Ros Yan <ros.yanikin@gmail.com>
2025-01-18 11:50:10 +0000

    new

--- src/components/js/main.js
+++ src/components/js/main.js
@@ -1,10 +1,10 @@
 $(function(){
     $('.section-3__slider').slick({
-        prevArrow: '<img class="section-3__slider-arrow slider__arrow-left" src="/src/img/section3/arrow-left.svg" alt="">',
-        nextArrow: '<img class="section-3__slider-arrow slider__arrow-right" src="/src/img/section3/arrow-right.svg" alt="">',
+        prevArrow: '<img class="section3-slider__arrow slider__arrow-left" src="../img/section3/arrow-left.svg" alt="">',
+        nextArrow: '<img class="section3-slider__arrow slider__arrow-right" src="../img/section3/arrow-right.svg" alt="">',
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 3, 
         dots: true,
     });
 });
